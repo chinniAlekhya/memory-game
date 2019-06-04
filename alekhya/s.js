765686246e9908c92e4f26c27a65cf39f724730b
chinniAlekhya <40908021+chinniAlekhya@users.noreply.github.com>
2019-06-04 09:55:52 +0530

    Add files via upload

---
+++ alekhya/s.js
@@ -0,0 +1,39 @@
+var images=["1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png","12.png"];
+var i,ii,randomIndex;
+function getImage(){
+var img='<img id="id11" src=\"';
+ randomIndex = Math.floor(Math.random() * images.length); 
+    img += images[randomIndex];
+    img += '\" alt=\"Some alt text\"/ width="200" height="200" >';
+    return img;
+    
+} 
+function myfunction(){
+  alert("time up!");
+  }
+function displayImage(){
+   document.write(getImage());
+   setTimeout(myfunction,4000);
+   var hidden = false;
+setInterval(function(){document.getElementById("id11").style.visibility= hidden ? "visible" : "hidden";},100);
+
+}
+displayImage();
+
+
+
+function clik(me){
+    i= me.id;
+    }
+ function check(){
+   ii=document.getElementsByTagName("img")[randomIndex+1].getAttribute("class");
+ if(i===ii){
+    alert("you won");    
+ }
+
+ else{
+    alert("you lose"+"\n"+"try again");
+ }
+}
+
+
